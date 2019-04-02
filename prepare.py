0fc263fc46aced755af3f61c2b1760ad54af9db7
Chad_Hackney <hackney.chad@gmail.com>
2019-04-02 16:48:00 -0500

    first logit model

---
+++ prepare.py
@@ -0,0 +1,14 @@
+
+import acquire as acq
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+
+def encode_churn(df):
+    encoder = LabelEncoder()
+    encoder.fit(df.churn)
+    return df.assign(churn_encode = encoder.transform(df.churn))
+    
+def prep_telco(df):
+    return df.pipe(encode_churn)
+

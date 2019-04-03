06791c3172b22e1016df0aba4b8b92560d73f3ae
Chad_Hackney <hackney.chad@gmail.com>
2019-04-03 10:45:47 -0500

    updating chad

--- prepare.py
+++ prepare.py
@@ -9,9 +9,29 @@ def encode_churn(df):
     encoder.fit(df.churn)
     return df.assign(churn_encode = encoder.transform(df.churn))
 
+# def encode_streaming_tv(df):
+#     encoder = LabelEncoder()
+#     encoder.fit(df.streaming_tv)
+#     return df.assign(streaming_tv_encode = encoder.transform(df.streaming_tv))
+
+# def encode_streaming_movies(df):
+#     encoder = LabelEncoder()
+#     encoder.fit(df.streaming_movies)
+#     return df.assign(streaming_movies_encode = encoder.transform(df.streaming_movies))
+
+# def encode_online_security(df):
+#     encoder = LabelEncoder()
+#     encoder.fit(df.online_security)
+#     return df.assign(online_security_encode = encoder.transform(df.online_security))
+
+# def encode_online_backup(df):
+#     encoder = LabelEncoder()
+#     encoder.fit(df.online_backup)
+#     return df.assign(online_backup_encode = encoder.transform(df.online_backup))
+
 def tenure_yearly(df):
     df[['tenure_yearly']] = df[['tenure']]//12
-    return df 
+    return df    
 
 def prep_telco(df):
     return df.pipe(encode_churn)

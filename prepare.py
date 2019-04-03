f2d9ac6582b89e11e7e92ebcb69c8e1c940cd05c
Norrick <Norrick.mcgee@gmail.com>
2019-04-03 09:34:33 -0500

    tenure_yearly added

--- prepare.py
+++ prepare.py
@@ -8,7 +8,10 @@ def encode_churn(df):
     encoder = LabelEncoder()
     encoder.fit(df.churn)
     return df.assign(churn_encode = encoder.transform(df.churn))
-    
+
+def tenure_yearly(df):
+    df[['tenure_yearly']] = df[['tenure']]//12
+    return df 
+
 def prep_telco(df):
     return df.pipe(encode_churn)
-

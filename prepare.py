79bbf9b98739777e56e13c145ece5877c1cd2a18
Chad_Hackney <hackney.chad@gmail.com>
2019-04-03 11:08:03 -0500

    another fix

--- prepare.py
+++ prepare.py
@@ -9,29 +9,8 @@ def encode_churn(df):
     encoder.fit(df.churn)
     return df.assign(churn_encode = encoder.transform(df.churn))
 
-# def encode_streaming_tv(df):
-#     encoder = LabelEncoder()
-#     encoder.fit(df.streaming_tv)
-#     return df.assign(streaming_tv_encode = encoder.transform(df.streaming_tv))
-
-# def encode_streaming_movies(df):
-#     encoder = LabelEncoder()
-#     encoder.fit(df.streaming_movies)
-#     return df.assign(streaming_movies_encode = encoder.transform(df.streaming_movies))
-
-# def encode_online_security(df):
-#     encoder = LabelEncoder()
-#     encoder.fit(df.online_security)
-#     return df.assign(online_security_encode = encoder.transform(df.online_security))
-
-# def encode_online_backup(df):
-#     encoder = LabelEncoder()
-#     encoder.fit(df.online_backup)
-#     return df.assign(online_backup_encode = encoder.transform(df.online_backup))
-
 def tenure_yearly(df):
     df[['tenure_yearly']] = df[['tenure']]//12
-    return df    
     return df
 
 def multiple_lines_encode(df):
@@ -40,7 +19,30 @@ def multiple_lines_encode(df):
     tdf['multiple_lines'].replace('No', '1', inplace = True)
     tdf['multiple_lines'].replace('Yes','2', inplace=True)
     tdf['multiple_lines']=tdf.multiple_lines.astype('int')
-    return tdf 
+    return tdf
+
+# def encode_streaming_tv(df):
+#     tdf = df.copy()
+#     tdf['streaming_tv'].replace('No internet service', '0', inplace = True)
+#     tdf['streaming_tv'].replace('No', '1', inplace = True)
+#     tdf['streaming_tv'].replace('Yes', '2', inplace = True)
+#     df['streaming_encode']=tdf.streaming_tv.astype('int')
+#     return df
+
+# def encode_streaming_movies(df):
+#     tdf = df.copy()
+#     tdf['streaming_movies'].replace('No internet service', '0', inplace = True)
+#     tdf['streaming_movies'].replace('No', '1', inplace = True)
+#     tdf['streaming_movies'].replace('Yes', '2', inplace = True)
+#     df['streaming_movies_encode']=tdf.streaming_tv.astype('int')
+#     return df
 
 def prep_telco(df):
-    return df.pipe(encode_churn)
+    return df.pipe(encode_churn)\
+    .pipe(multiple_lines_encode)
+    # \
+    # .pipe(encode_streaming_tv)
+    # # \
+    # .pipe(encode_streaming_movies)
+
+            

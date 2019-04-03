a80b8b03d11613343b7397b80a31130365a598ca
Norrick <Norrick.mcgee@gmail.com>
2019-04-03 11:27:10 -0500

    Streaming encode added

--- prepare.py
+++ prepare.py
@@ -18,8 +18,28 @@ def multiple_lines_encode(df):
     tdf['multiple_lines'].replace('No phone service', '0', inplace = True)
     tdf['multiple_lines'].replace('No', '1', inplace = True)
     tdf['multiple_lines'].replace('Yes','2', inplace=True)
-    tdf['multiple_lines']=tdf.multiple_lines.astype('int')
-    return tdf
+    df['phone_id'] = tdf.multiple_lines.astype('int')
+    return df
+
+def streaming_movies_encode(df):
+    tdf = df.copy()
+    tdf['streaming_movies'].replace('No internet service', '0', inplace = True)
+    tdf['streaming_movies'].replace('No', '0', inplace = True)
+    tdf['streaming_movies'].replace('Yes','1', inplace=True)
+    df['movies_encode'] = tdf.streaming_movies.astype('int')
+    return df
+
+def streaming_tv_encode(df):
+    tdf = df.copy()
+    tdf['streaming_tv'].replace('No internet service', '0', inplace = True)
+    tdf['streaming_tv'].replace('No', '0', inplace = True)
+    tdf['streaming_tv'].replace('Yes','1', inplace=True)
+    df['tv_encode'] = tdf.streaming_tv.astype('int')
+    return df
+
+def streaming_combine(df):
+    df['streaming_services'] = df.tv_encode + df.movies_encode
+    return df
 
 # def encode_streaming_tv(df):
 #     tdf = df.copy()
@@ -39,10 +59,7 @@ def multiple_lines_encode(df):
 
 def prep_telco(df):
     return df.pipe(encode_churn)\
-    .pipe(multiple_lines_encode)
-    # \
-    # .pipe(encode_streaming_tv)
-    # # \
-    # .pipe(encode_streaming_movies)
-
-            
+    .pipe(multiple_lines_encode)\
+    .pipe(streaming_movies_encode)\
+    .pipe(streaming_tv_encode)\
+    .pipe(streaming_combine)
